3cdb2d2ecd5e05961b76f90af3c93afc89e34bb4
hyeonji7557 <hyeunji7557@naver.com>
2021-11-25 01:38:21 +0900

    [Refactor] 나머지_10430

--- src/나머지_10430.java
+++ src/나머지_10430.java
@@ -4,13 +4,13 @@ public class 나머지_10430 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=sc.nextInt();
-		System.out.println((a+b)%c);
-		System.out.println((a%c)+(b%c)%c);
-		System.out.println((a*b)%c);
-		System.out.println((a%c)*(b%c)%c);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
+		System.out.println((A+B)%C);
+		System.out.println(((A%C)+(B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
 		sc.close();
 	}
 }
