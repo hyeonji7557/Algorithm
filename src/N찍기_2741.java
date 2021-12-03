f65eba69c466be261d041b0c385b11de8670f348
hyeonji7557 <hyeunji7557@naver.com>
2021-12-03 18:07:35 +0900

    [ADD] BOJ 2741

---
+++ src/N찍기_2741.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N찍기_2741 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int n = Integer.parseInt(str);
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+		}
+	}
+}
