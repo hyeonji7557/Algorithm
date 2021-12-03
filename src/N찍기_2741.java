5cd2e57cb368792c36f24dccb5c05119c8a40e24
hyeonji7557 <hyeunji7557@naver.com>
2021-12-03 18:54:42 +0900

    [Refactor] BOJ 2741

--- src/N찍기_2741.java
+++ src/N찍기_2741.java
@@ -7,9 +7,10 @@ public class N찍기_2741 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(str);
-		for(int i=1;i<=n;i++) {
-			System.out.println(i);
-		}
+		for(int i=1;i<=n;i++)
+			sb.append(i).append("\n");
+		System.out.println(sb);
 	}
 }
