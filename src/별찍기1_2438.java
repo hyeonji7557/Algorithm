65d8652d533181ef3028135dbda1a73f17ee616b
hyeonji7557 <hyeunji7557@naver.com>
2021-12-05 23:35:23 +0900

    [Refactor] BOJ 2438

--- src/별찍기1_2438.java
+++ src/별찍기1_2438.java
@@ -4,11 +4,11 @@ import java.io.InputStreamReader;
 
 public class 별찍기1_2438 {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		for (int i=1; i<=n; i++) {
-			for (int j=1; j<=i; j++) 
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++)
 				System.out.print("*");
 			System.out.println();
 		}
