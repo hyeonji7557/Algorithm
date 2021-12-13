dda1638c96fbfe46545f088219efbff30fcbdd87
hyeonji7557 <hyeunji7557@naver.com>
2021-12-13 23:15:29 +0900

    [Refactor] BOJ 10951

--- src/A더하기B빼기4_10951.java
+++ src/A더하기B빼기4_10951.java
@@ -8,10 +8,9 @@ public class A더하기B빼기4_10951 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		String str;
 		while ((str = br.readLine()) != null) {
-			st = new StringTokenizer(str, " ");
+			StringTokenizer st = new StringTokenizer(str);
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			sb.append(a + b).append("\n");
