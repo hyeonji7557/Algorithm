269fd60932619f6d62f37ea45c0ae09dedbd875d
hyeonji7557 <hyeunji7557@naver.com>
2021-12-14 19:59:43 +0900

    [Refactor] BOJ 2562

--- src/최댓값_2562.java
+++ src/최댓값_2562.java
@@ -3,8 +3,8 @@ import java.io.InputStreamReader;
 import java.io.IOException;
  
 public class 최댓값_2562 {
+	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] arr = new int[9];
 		for(int i = 0 ; i < 9 ; i++) 
@@ -20,7 +20,6 @@ public class 최댓값_2562 {
 				index = count;
 			}
 		}
-		System.out.println(max);
-		System.out.println(index);
+		System.out.println(max + "\n" + index);
 	}
 }
