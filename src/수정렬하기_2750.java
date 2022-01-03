66e89b99ff944b40553fe595be31f75becb963de
leehyeonji <hyeunji7557@naver.com>
2022-01-03 22:58:04 +0900

    [ADD] BOJ 2750

---
+++ src/수정렬하기_2750.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+ 
+public class 수정렬하기_2750 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		for(int val : arr)
+			sb.append(val).append('\n');
+		System.out.println(sb);
+	}
+}
