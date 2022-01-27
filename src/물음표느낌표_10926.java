04608d5e8f8f55ecc48586c6d63deef3c9cebed9
leehyeonji <hyeunji7557@naver.com>
2022-01-27 21:28:28 +0900

    [ADD] BOJ 10926

---
+++ src/물음표느낌표_10926.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 물음표느낌표_10926 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "??!");
+	}
+}
