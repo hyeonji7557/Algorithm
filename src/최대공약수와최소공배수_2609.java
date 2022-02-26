2dac95b840fbebf4dde8bcc1f267e8c5fefa0344
hyeonji7557 <hyeunji7557@naver.com>
2022-02-26 21:42:55 +0900

    [ADD] BOJ 2609

---
+++ src/최대공약수와최소공배수_2609.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대공약수와최소공배수_2609 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = gcd(a, b);
+		System.out.println(c);
+		System.out.println(a * b / c);
+	}
+	
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
