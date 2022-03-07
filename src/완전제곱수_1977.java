aa6f5ede587c69befcb0231562b2a49b31937608
hyeonji7557 <hyeunji7557@naver.com>
2022-03-07 20:42:09 +0900

    [ADD] BOJ 1977

---
+++ src/완전제곱수_1977.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 완전제곱수_1977 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for (int i = m; i <= n; i++) {
+			for (int j = 1; j <= 100; j++) {
+				if (i == j * j) {
+					sum += i;
+					if (i < min)
+						min = i;
+				}
+			}
+		}
+		if (sum == 0)
+			System.out.println(-1);
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+	}
+}
