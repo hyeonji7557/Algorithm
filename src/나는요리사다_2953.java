9cc2644be7ce914f98b210c6f8e92049ebb68da2
hyeonji7557 <hyeunji7557@naver.com>
2022-03-11 15:03:36 +0900

    [Refactor] BOJ 2953

--- src/나는요리사다_2953.java
+++ src/나는요리사다_2953.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 나는요리사다_2953 {
@@ -9,7 +8,6 @@ public class 나는요리사다_2953 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] arr = new int[5];
-		int sum = 0;
 		for (int i = 0; i < 5; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 4; j++) 
