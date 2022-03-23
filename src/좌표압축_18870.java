818a2a990ce2acb04b09a99ab8a94876da5a1366
hyeonji7557 <hyeunji7557@naver.com>
2022-03-23 21:42:46 +0900

    [ADD] BOJ 18870

---
+++ src/좌표압축_18870.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class 좌표압축_18870 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int[] origin = new int[n];
+		int[] sorted = new int[n];
+		HashMap<Integer, Integer> Map = new HashMap<Integer, Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			sorted[i] = origin[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(sorted);
+		int rank = 0;
+		for (int v : sorted) {
+			if (!Map.containsKey(v)) {
+				Map.put(v, rank);
+				rank++;
+			}
+		}
+		for (int key : origin) {
+			int ranking = Map.get(key);
+			sb.append(ranking).append(' ');
+		}
+		System.out.println(sb);
+	}
+}
