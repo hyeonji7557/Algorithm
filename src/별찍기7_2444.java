1110e15d4c6fcc717e3895f1cdf11f1d30f42608
hyeonji7557 <hyeunji7557@naver.com>
2022-03-29 17:54:22 +0900

    [Refactor] BOJ 2444

--- src/별찍기7_2444.java
+++ src/별찍기7_2444.java
@@ -10,14 +10,14 @@ public class 별찍기7_2444 {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 0; j < n - i; j++)
 				System.out.print(" ");
-			for (int j = 0; j < i * 2 - 1; j++)
+			for (int k = 0; k < i * 2 - 1; k++)
 				System.out.print("*");
 			System.out.println();
 		}
 		for (int i = n - 1; i >= 0; i--) {
 			for (int j = 0; j < n - i; j++)
 				System.out.print(" ");
-			for (int j = 0; j < i * 2 - 1; j++)
+			for (int k = 0; k < i * 2 - 1; k++)
 				System.out.print("*");
 			System.out.println();
 		}
