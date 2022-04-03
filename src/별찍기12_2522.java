5270303054f0d2b401e1e73de6b4023fff563d30
hyeonji7557 <hyeunji7557@naver.com>
2022-04-03 18:40:00 +0900

    [ADD] BOJ 2522

---
+++ src/별찍기12_2522.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 별찍기12_2522 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++)
+				System.out.print(" ");
+			for (int k = 1; k <= i; k++)
+				System.out.print("*");
+			System.out.println();
+		}
+		for (int i = 1; i <= n - 1; i++) {
+			for (int j = 1; j <= i; j++)
+				System.out.print(" ");
+			for (int k = 1; k <= n - i; k++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
