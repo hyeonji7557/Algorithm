db8f141692b06466b0b5463b788f5dfccdecf0ce
LeeHyeonJi <hyeunji7557@naver.com>
2022-11-09 19:13:36 +0900

    [ADD] PRO 머쓱이보다키큰사람

---
+++ src/Lv0_머쓱이보다키큰사람.java
@@ -0,0 +1,12 @@
+
+public class Lv0_머쓱이보다키큰사람 {
+	public int solution(int[] array, int height) {
+		int answer = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > height) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
