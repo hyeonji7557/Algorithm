fb8f680c37f1d70ed2f1e65992ff53cd93e3e1c9
LeeHyeonJi <hyeunji7557@naver.com>
2022-11-09 19:09:42 +0900

    [ADD] PRO 양꼬치

---
+++ src/Lv0_양꼬치.java
@@ -0,0 +1,12 @@
+
+public class Lv0_양꼬치 {
+	public int solution(int n, int k) {
+		int answer = 0;
+		int a = 0;
+		if (n >= 10) {
+			a = n / 10;
+		}
+		answer = n * 12000 + k * 2000 - a * 2000;
+		return answer;
+	}
+}
