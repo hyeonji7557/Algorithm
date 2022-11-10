0894c85072e980bfca5641d5fe8e94c166c07d3b
LeeHyeonJi <hyeunji7557@naver.com>
2022-11-10 18:32:21 +0900

    [ADD] PRO 배열원소의길이

---
+++ src/Lv0_배열원소의길이.java
@@ -0,0 +1,11 @@
+
+public class Lv0_배열원소의길이 {
+	public int[] solution(String[] strlist) {
+		int[] answer = new int[strlist.length];
+		for (int i = 0; i < strlist.length; i++) {
+			int a = strlist[i].length();
+			answer[i] = a;
+		}
+		return answer;
+	}
+}
