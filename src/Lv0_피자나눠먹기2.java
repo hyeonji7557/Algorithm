b3db9fe92c90b4085be946c800cfbe65ade1be8a
LeeHyeonJi <hyeunji7557@naver.com>
2022-11-15 17:35:12 +0900

    [ADD] PRO 피자나눠먹기2

---
+++ src/Lv0_피자나눠먹기2.java
@@ -0,0 +1,13 @@
+
+public class Lv0_피자나눠먹기2 {
+	public int solution(int n) {
+		int answer = 0;
+		for (int i = 1; i < 10; i++) {
+			if ((n * i) % 6 == 0) {
+				answer = (n * i) / 6;
+				break;
+			}
+		}
+		return answer;
+	}
+}
