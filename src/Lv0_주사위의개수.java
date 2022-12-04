6ba063e0a6a9ffd0002929c7d06f8d587df129ba
LeeHyeonJi <hyeunji7557@naver.com>
2022-12-04 22:48:52 +0900

    [ADD] PRO 주사위의개수

---
+++ src/Lv0_주사위의개수.java
@@ -0,0 +1,10 @@
+
+public class Lv0_주사위의개수 {
+	public int solution(int[] box, int n) {
+		int answer = 1;
+		for (int i = 0; i < box.length; i++) {
+			answer *= (box[i] / n);
+		}
+		return answer;
+	}
+}
