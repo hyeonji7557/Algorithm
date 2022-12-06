159201e91aee27d344021b168cd084840ae9b21d
LeeHyeonJi <hyeunji7557@naver.com>
2022-12-06 23:14:32 +0900

    [ADD] PRO 최댓값만들기1

---
+++ src/Lv0_최댓값만들기1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Lv0_최댓값만들기1 {
+	public int solution(int[] numbers) {
+		int answer;
+		Arrays.sort(numbers);
+		answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+		return answer;
+	}
+}
