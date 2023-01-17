3ed601da87a35f3988ed033c0d3a4331916002b8
LeeHyeonJi <hyeunji7557@naver.com>
2023-01-17 21:29:27 +0900

    [ADD] PRO 가장큰수찾기

---
+++ src/Lv0_가장큰수찾기.java
@@ -0,0 +1,15 @@
+
+public class Lv0_가장큰수찾기 {
+	public int[] solution(int[] array) {
+		int[] answer = new int[2];
+		int max = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+				answer[1] = i;
+			}
+		}
+		answer[0] = max;
+		return answer;
+	}
+}
