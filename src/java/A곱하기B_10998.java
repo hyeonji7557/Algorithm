459e0a4c611409209bd757b4fbedef1a36a79333
LeeHyeonJi <hyeunji7557@naver.com>
2023-05-23 23:13:10 +0900

    move folder

--- src/java/A곱하기B_10998.java
+++ src/java/A곱하기B_10998.java
@@ -1,13 +1,14 @@
+package java;
 
 import java.util.Scanner;
 
-public class A빼기B_1001 {
+public class A곱하기B_10998 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-		System.out.println(a-b);
+		System.out.println(a*b);
 		sc.close();
 	}
 }
--- src/java/A나누기B_1008.java
+++ src/java/A나누기B_1008.java
@@ -1,13 +1,14 @@
+package java;
 
 import java.util.Scanner;
 
-public class A곱하기B_10998 {
+public class A나누기B_1008 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-		System.out.println(a*b);
+		System.out.println((double)a/b);
 		sc.close();
 	}
 }
