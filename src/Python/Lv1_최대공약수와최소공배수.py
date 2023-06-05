4d8c8c125f08e636c8871c5061ce197e74bf1f93
이현지 <94505650+hyeonji7557@users.noreply.github.com>
2023-06-05 23:41:37 +0900

    [ADD] PRO 최대공약수와최소공배수

--- src/Python/Lv1_최대공약수와최소공배수.py
+++ src/Python/Lv1_최대공약수와최소공배수.py
@@ -1,3 +1,8 @@
 def solution(n, m):
-    answer = []
-    return answer
+    def gcd(n, m):
+        while m:
+            n, m = m, n % m
+        return n
+    def lcm(n, m):
+        return (n * m) // gcd(n, m)
+    return [gcd(n, m), lcm(n, m)]
